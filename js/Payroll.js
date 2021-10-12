c18ad9828995ddd2b54c0545ae5f7c5e4facb814
Akash Singh <akashsingh6280@gmail.com>
2021-10-12 17:29:43 +0530

    ADDED: js file path added in head

--- js/Payroll.js
+++
@@ -1,5 +0,0 @@
-var output = document.querySelector(".salary-output");
-var salary = document.querySelector("#salary");
-salary.addEventListener("input", function () {
-  output.textContent = salary.value;
-});
