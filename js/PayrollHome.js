68f729aab59e1a13f3971c2d0b8ccba0ba8198ed
Akash Singh <akashsingh6280@gmail.com>
2021-10-14 06:53:40 +0530

    REFACTORED: To display detail from local storage

--- js/PayrollHome.js
+++ js/PayrollHome.js
@@ -1,8 +1,19 @@
+let employeePayrollList;
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 window.addEventListener("DOMContentLoaded", (event) => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
   });
 
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem("EmployeePayrollList") ? 
+    JSON.parse(localStorage.getItem("EmployeePayrollList")) : [];
+}; 
+
 const createInnerHtml = () => {
+    if (employeePayrollList.length == 0) return;
     const headerHtml = `<tr>
                         <th></th>
                         <th>Name</th>
@@ -13,8 +24,9 @@ const createInnerHtml = () => {
                         <th>Actions</th>
                         </tr>`;
     let innerHtml=`${headerHtml}`;
-    let employeePayrollList=createEmployeePayrollJSON();
-    for(const employeePayrollData of employeePayrollList){              
+    for(const employeePayrollData of employeePayrollList){   
+        let date=new Date(employeePayrollData._startDate); 
+        date=date.getDate() + " " + months[(date.getMonth() + 1)] + " " + date.getFullYear()   
         innerHtml = `${innerHtml}
                         <tr>
                             <td><img class="profile" alt="" src="${employeePayrollData._profile}"></td>
@@ -23,7 +35,7 @@ const createInnerHtml = () => {
                             <td>${getDeptHtml(employeePayrollData._department)}
                             </td>
                             <td>${employeePayrollData._salary}</td>
-                            <td>${employeePayrollData._startDate}</td>
+                            <td>${date}</td>
                             <td>
                                 <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                                 <img id="1" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
