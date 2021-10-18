93298eab0aaf3f7ad437f321347a20f1f5f6f462
Akash Singh <akashsingh6280@gmail.com>
2021-10-18 22:43:51 +0530

    ADDED: function to validate name and date

--- js/utility.js
+++ js/utility.js
@@ -4,3 +4,14 @@ const stringifyDate = (date) => {
                     new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
     return newDate;
 }
+const checkName = (name) => {
+    let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(!nameRegex.test(name)) throw 'Name is Incorrect!';
+}
+
+const checkStartDate = (startDate) => {
+    if(startDate > new Date())
+        throw 'Start Date can not be a future date';
+    if( Date.now()-startDate >30*24*60*60*1000)
+        throw 'Start Date is beyond 30 days';
+} 
