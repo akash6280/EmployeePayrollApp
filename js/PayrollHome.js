47fcc02adc0d72e22680943075a534d697b37204
Akash Singh <akashsingh6280@gmail.com>
2021-10-18 22:45:42 +0530

    REFACTORED: delinked reference to emp_id

--- js/PayrollHome.js
+++ js/PayrollHome.js
@@ -34,8 +34,8 @@ const createInnerHtml = () => {
                             <td>${employeePayrollData._salary}</td>
                             <td>${stringifyDate(employeePayrollData._startDate)}</td>
                             <td>
-                                <img id="${employeePayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                                <img id="${employeePayrollData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+                                <img id="${employeePayrollData.id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                                <img id="${employeePayrollData.id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
                             </td>
                         </tr>
                       `;
@@ -52,11 +52,11 @@ const getDeptHtml = (deptList) => {
 };
 
 const remove = (node) => {
-    let employeePayrollData = employeePayrollList.find(empData => empData._id == node.id);
+    let employeePayrollData = employeePayrollList.find(empData => empData.id == node.id);
     if(!employeePayrollData) return;
     const index = employeePayrollList
-                 .map(empData => empData._id)
-                 .indexOf(employeePayrollData._id);
+                 .map(empData => empData.id)
+                 .indexOf(employeePayrollData.id);
     employeePayrollList.splice(index, 1);
     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
     document.querySelector(".emp-count").textContent = employeePayrollList.length;
@@ -64,7 +64,7 @@ const remove = (node) => {
 } ;
 
 const update = (node) => {
-    let employeePayrollData = employeePayrollList.find(empData => empData._id == node.id);
+    let employeePayrollData = employeePayrollList.find(empData => empData.id == node.id);
     if(!employeePayrollData) return;
     localStorage.setItem('editEmp',JSON.stringify(employeePayrollData));
     window.location.replace(site_properties.add_emp_payroll_page);
